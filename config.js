a7705455df482187004a9ac120244c7acaaff662
alirohmat <71525350+alirohmat@users.noreply.github.com>
2025-03-19 07:08:52 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
-global.owner = ['6285842647866']  
-global.mods = ['6285842647866'] 
-global.prems = ['6285842647866']
-global.nameowner = 'Lann'
-global.numberowner = '6285842647866'
-global.mail = 'support@tioprm.eu.org' 
-global.gc = 'https://chat.whatsapp.com/G4f1fTpz9zL4EH3FyIcaPR'
-global.instagram = 'https://instagram.com/erlanrahmat_14'
-global.wm = '© Tio'
+global.owner = ['6282132789470']  
+global.mods = ['6282132789470'] 
+global.prems = ['6282132789470']
+global.nameowner = 'Brogalan'
+global.numberowner = '6282132789470'
+global.mail = 'admin@siap.biz.id' 
+global.gc = 'https://t.me/bakulanvpn'
+global.instagram = 'https://instagram.com/rodo_merana'
+global.wm = '© ALI ROHMATULLOH'
 global.wait = '_*Tunggu sedang di proses...*_'
 global.eror = '_*Server Error*_'
 global.stiker_wait = '*⫹⫺ Stiker sedang dibuat...*'
@@ -16,11 +16,11 @@ global.maxwarn = '2' // Peringatan maksimum
 global.antiporn = true // Auto delete pesan porno (bot harus admin)
 
 //INI WAJIB DI ISI!//
-global.lann = 'YOUR_APIKEY_HERE' 
+global.lann = 'Btz-jdyXQ' 
 //Daftar terlebih dahulu https://api.betabotz.eu.org
 
 //INI OPTIONAL BOLEH DI ISI BOLEH JUGA ENGGA//
-global.btc = 'YOUR_APIKEY_HERE'
+global.btc = 'Btz-jdyXQ'
 //Daftar https://api.botcahx.eu.org 
 
 global.APIs = {   
